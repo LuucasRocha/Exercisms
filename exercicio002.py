b67b473e0f4c49516d8d867eb9727274faa43c2b
LuucasRocha <lucasrocha@ucl.br>
2025-10-03 02:48:21 -0300

    Exercício 002 adicionado.

---
+++ exercicio002.py
@@ -0,0 +1,15 @@
+# Exercício 002 - Um comerciante comprou um produto e quer vendê-lo com um lucro de 45% se o
+#valor da compra for menor que R$ 20,00; caso contrário, o lucro será de 30%.
+#Elabore um algoritmo que leia o valor do produto e imprima o valor de venda para
+#o produto.
+
+valor_compra = float(input("Digite o valor da compra: R$ "))
+if valor_compra < 20.00:
+    lucro = 0.45
+else:
+    lucro = 0.30
+
+valor_venda = valor_compra * (1 + lucro)
+print(f"O valor de venda para o produto é: R$ {valor_venda:.2f}")
+
+
