936370fc4d465f1aaac50298e35fcba35fc067a6
LuucasRocha <lucasrocha@ucl.br>
2025-10-05 00:30:46 -0300

    Exercicio 004 adicionado

---
+++ exercicio004.py
@@ -0,0 +1,11 @@
+"""Criar um programa que imprima todos os números inteiros de 1 até um limite definido pelo usuário."""
+
+limite = int(input("Defina um limite superior: ")) 
+lista_numeros = []
+
+for i in range(1, limite + 1):
+    lista_numeros.append(i)
+    
+print(f"Os números inteiros entre 1 e o limite superior definido é: {lista_numeros}")
+
+
