7aa20734d9b9c1a76f76633ff5029cff52a8098c
LuucasRocha <lucasrocha@ucl.br>
2025-10-22 22:39:07 -0300

    melhoria-17

--- exercicio017.py
+++ exercicio017.py
@@ -1,7 +1,10 @@
 # Exercício 17
 # Crie um programa que leia o nome completo de uma pessoa e mostre:
 # - O nome com todas as letras maiúsculas e minúsculas.
-nome = input("Digite seu nome completo: ")
 
-print("Nome em maiúsculas:", nome.upper())
-print("Nome em minúsculas:", nome.lower())
+nome_completo = input("Digite seu nome completo: ")
+print("Nome em maiúsculas:", nome_completo.upper())
+print("Nome em minúsculas:", nome_completo.lower())
+# - Quantas letras ao todo (sem considerar espaços).
+letras_sem_espacos = len(nome_completo.replace(" ", ""))
+print("Total de letras (sem espaços):", letras_sem_espacos)
