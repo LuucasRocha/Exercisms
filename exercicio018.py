30a714ba4e766732f959af7f9e7121af2228f78a
LuucasRocha <lucasrocha@ucl.br>
2025-10-22 22:37:41 -0300

    melhoria-ex-18

--- exercicio018.py
+++ exercicio018.py
@@ -1,9 +1,11 @@
 # Criar um programa que faça um pequeno dicionário com as seguintes informações: nome, idade, cidade.
+
 usuario = {
     "nome": input("Digite seu nome: "),
     "idade": int(input("Digite sua idade: ")),
-    "cidade": input("Digite sua cidade: ")
+    "cidade": input("Digite o nome da sua cidade: ")
 }
 
-print("Dicionário criado:")
-print(usuario)
+print("As informações do usuário são:")
+for chave, valor in usuario.items():
+    print(f"{chave.capitalize()}: {valor}")
