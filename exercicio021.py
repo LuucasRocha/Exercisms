3aaa37fe7f573bc9fb5c9ea27f579fd49c90b1ef
LuucasRocha <lucasrocha@ucl.br>
2025-10-26 02:41:44 -0300

    add

--- exercicio021.py
+++ exercicio021.py
@@ -1,7 +1,6 @@
 #Crie uma função soma_pares(lista) que receba uma lista de números e retorne a soma somente dos pares
 
 def soma_pares(lista):
-   return sum(num for num in lista if num % 2 == 0) 
-    
-lista = [1,2,3,4,5,6,7]
-print(soma_pares(lista))
+    return sum(num for num in lista if num % 2 == 0)
+
+print(soma_pares([]))
