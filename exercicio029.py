ef704614959ad2c6cb03c64e46250b4af14ffffa
LuucasRocha <lucasrocha@ucl.br>
2025-10-28 06:56:22 -0300

    add 029

--- exercicio029.py
+++ exercicio029.py
@@ -5,10 +5,16 @@ from collections import Counter
 
 def mais_comum(lista):
     if not lista:
-        return "Lista vazia"
+        return "Lista vazia."
     cont = Counter(lista)
-    mais_frequente = cont.most_common(1)[0][0]
-    return mais_frequente
+    mais_comum = max(cont.values())
+    for item in lista:
+        if cont[item] == mais_comum:
+            return item
 
-print(mais_comum([2,2,3,3,3,4,5,6]))  # → 3
-print(mais_comum([]))                 # → "Lista vazia"
+entrada = input("Digite uma lista de números separados por espaço: ")
+
+lista_numeros = [int(x) for x in entrada.split()]
+
+print(f"Número mais frequente da lista: {mais_comum(lista_numeros)}")
+    
