9cd814892e1570b71ad63bb355056fe6faae7e52
LuucasRocha <lucasrocha@ucl.br>
2025-11-19 09:33:35 -0300

    add

--- exercicio033.py
+++ exercicio033.py
@@ -1,19 +1,11 @@
 # Crie um fluxo para decidir se uma pessoa pode votar (idade >= 16).
 
-def autentica_votacao(idade):
+def autentica_vot(idade):
+    if not isinstance(idade, int):
+        return None
     return True if idade >=16 else False
 
-while True:
-    nome = input("Digite seu nome: ")
+entrada_idade = 20
 
-    try:
-        idade = int(input("Digite sua idade: "))
-
-        verifica_vot = autentica_votacao(idade)
-        if verifica_vot:
-            print(f"Eleitor: {nome} \nStatus Votação: Ok")
-        else:
-            print(f"Eleitor: {nome} \nStatus Votação: Negado")
-
-    except ValueError:
-        print("Idade inválida, digite um número.")
+status_vot = autentica_vot(entrada_idade)
+print(f"Status votação: {status_vot}")
