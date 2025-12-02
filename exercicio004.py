31f8725d2b6956eb3684d4c3cfbff7a0f370e8ee
Lucas Rocha <lucasrocha@ucl.br>
2025-12-02 00:06:48 -0300

    modifield

--- exercicio004.py
+++ exercicio004.py
@@ -1,14 +1,10 @@
 """Criar um programa que imprima todos os números inteiros de 1 até um limite definido pelo usuário."""
 
-limite = int(input("Defina um limite superior: ")) 
-lista_numeros = []
+def imprime_inteiros(n):
+    for i in range(1, n + 1):
+        print(i)
 
-for i in range(1, limite + 1):
-    lista_numeros.append(i)
-    
-print(f"Os números inteiros entre 1 e o limite superior definido é: {lista_numeros}")
-print("Fim do programa.")
-
+imprime_inteiros(40)
 
 
 
