c2bf63c4da01f501c357cd69e01ce84fb99abd2f
Lucas Rocha <lucasrocha@ucl.br>
2025-12-05 02:31:20 -0300

    modifield

--- exercicio033.py
+++ exercicio033.py
@@ -1,11 +1,10 @@
 # Crie um fluxo para decidir se uma pessoa pode votar (idade >= 16).
 
-def autentica_vot(idade):
+def verifica_idade_voto(idade):
     if not isinstance(idade, int):
         return None
-    return True if idade >=16 else False
+    return True if idade >= 16 else False
 
-entrada_idade = 20
+entrada = 16
 
-status_vot = autentica_vot(entrada_idade)
-print(f"Status votação: {status_vot}")
+print(verifica_idade_voto(entrada))
