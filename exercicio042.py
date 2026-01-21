3791bfa025fb99d01e9f951fb00a804f4e2973a6
Lucas Rocha <lucasrocha@ucl.br>
2026-01-20 21:16:31 -0300

    add

--- exercicio042.py
+++ exercicio042.py
@@ -6,45 +6,42 @@ Removam duplicados
 Contem frequência de elementos
 '''
 
-def maior_elemento_lista(lista):
-    return max(lista)
+def maior_elemento(lista):
+    maior = lista[0]
+    
+    for numero in lista:
+        if numero > maior:
+            maior = numero
+    
+    return maior
 
-def menor_elemento_lista(lista):
+def menor_elemento(lista):
     return min(lista)
 
-def media_elementos_lista(lista):
-    return sum(lista) / len(lista) if len(lista) != 0 else 0
-
-def lista_pares(lista):
-    pares = []
-    for numero in lista:
-        if numero % 2 == 0:
-            pares.append(numero)
-    return pares
+def media_lista(lista):
+    if len(lista) > 0:
+        return sum(lista) / len(lista)
+    else:
+        return None
 
 def remover_duplicados(lista):
-    nova_lista = []
-    for num in lista:
-        if num not in nova_lista:
-            nova_lista.append(num)
-    return nova_lista
-
-def remover_duplicados_set(lista):
     return list(set(lista))
 
-def remover_duplicados_set_com_ordem(lista):
-    return list(dict.fromkeys(lista))
-
 def frequencia_elementos(lista):
-    frequencia = {}
+    frequencia = dict()
+    
     for numero in lista:
-        if numero in frequencia:
-            frequencia[numero] += 1
-        else:
+        if numero not in frequencia:
             frequencia[numero] = 1
-    return frequencia 
+        else:
+            frequencia[numero] += 1
+    
+    return frequencia
 
-lista_entrada = [5, 4, 4, 3, 3, 3, 3, 3, 2, 2, 2, 1]
+lista_entrada = []
+
+
+
 
 
 
