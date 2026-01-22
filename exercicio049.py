448c29afaa1a89333603be4660a38ea1209eda0f
Lucas Rocha <lucasrocha@ucl.br>
2026-01-22 20:07:26 -0300

    add

--- exercicio049.py
+++ exercicio049.py
@@ -1,19 +1,14 @@
 #Gere uma lista de inteiros aleatórios e: quantas vezes o valor 9 aparece; em que posição está o valor 3; os valores pares.
+
 import random
 
-lista_aleatoria = [random.randint(1,10) for i in range(10)]
+lista_aleatoria = [random.randint(1, 10) for i in range(5)]
 
-for num in lista_aleatoria:
-    if 9 in lista_aleatoria:
-       contagem_nove = lista_aleatoria.count(9)
-    else:
-        contagem_nove = None
+posicoes_3 = [i for i, num in enumerate(lista_aleatoria) if num == 3]
 
-posicoes_3 = [i + 1 for i, num in enumerate(lista_aleatoria) if num == 3]
+contagem_nove = lista_aleatoria.count(9)
 
 print(lista_aleatoria)
-print(f"Contagem do valor 9: {contagem_nove}")
-print(f"Posições do valor 3: {posicoes_3}")   
-
-
+print(contagem_nove)
+print(posicoes_3)
         
