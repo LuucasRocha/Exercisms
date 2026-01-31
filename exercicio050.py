bdd421c3bce38e83d736314b0865fb2c1f98c52f
Lucas Rocha <lucasrocha@ucl.br>
2026-01-31 19:22:28 -0300

    add

--- exercicio050.py
+++ exercicio050.py
@@ -1,9 +1,19 @@
-palavras = ['aprender','jogar', 'lucas', 'amanda']
+#6. Faça um algoritmo que leia o tempo de duração de um evento em uma fábrica expressa em segundos e mostre-o expresso em horas, minutos e segundos.
 
-for palavra in palavras:
-    print(f"\nNa palavra {palavra.upper()} temos ", end='')
-    vogais = 'aeiou'
-    for letra in palavra:
-        if letra.lower() in vogais:
-            print(letra, end=' ')
+import random
+
+def converter_tempo(segundos):
+    horas = segundos // 3600
+    minutos = (segundos % 3600) // 60
+    segundos_restantes = segundos % 60
+    return horas, minutos, segundos_restantes
+
+tempo_aleatorio = random.randint(1, 10000)
+conversao = converter_tempo(tempo_aleatorio)
+
+print(f"Tempo em segundos: {tempo_aleatorio}")
+print(f"Convertido: {conversao[0]} horas, {conversao[1]} minutos e {conversao[2]} segundos")
+
+
+
         
